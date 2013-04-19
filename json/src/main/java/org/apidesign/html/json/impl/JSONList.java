e1ae17668ac2e2b586b43e2a366a1032126d4990
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-04-19 11:29:49 +0200

    Proper implementation of the list copied from KOList

--- json/src/main/java/org/apidesign/html/json/impl/JSONList.java
+++ json/src/main/java/org/apidesign/html/json/impl/JSONList.java
@@ -21,26 +21,148 @@
 package org.apidesign.html.json.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  *
  * @author Jaroslav Tulach <jtulach@netbeans.org>
  */
 public final class JSONList<T> extends ArrayList<T> {
+    private final String name;
+    private final String[] deps;
+    private Bindings model;
+    private Runnable onchange;
 
-    public JSONList(String prop) {
+    public JSONList(String name, String... deps) {
+        this.name = name;
+        this.deps = deps;
     }
-    
+
+    public void assign(Bindings model) {
+        if (this.model != model) {
+            this.model = model;
+            notifyChange();
+        }
+    }
+
     public JSONList<T> onChange(Runnable r) {
+        if (this.onchange != null) {
+            throw new IllegalStateException();
+        }
+        this.onchange = r;
         return this;
     }
 
-    public JSONList<T> assign(Bindings ko) {
-        return this;
-    }
-    
     @Override
-    public JSONList<T> clone() {
-        return this;
+    public boolean add(T e) {
+        boolean ret = super.add(e);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        boolean ret = super.addAll(c);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        boolean ret = super.addAll(index, c);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean ret = super.remove(o);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        notifyChange();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean ret = super.removeAll(c);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean ret = super.retainAll(c);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        T ret = super.set(index, element);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        super.add(index, element);
+        notifyChange();
+    }
+
+    @Override
+    public T remove(int index) {
+        T ret = super.remove(index);
+        notifyChange();
+        return ret;
+    }
+
+    @Override
+    public String toString() {
+        Iterator<T> it = iterator();
+        if (!it.hasNext()) {
+            return "[]";
+        }
+        String sep = "";
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        while (it.hasNext()) {
+            T t = it.next();
+            sb.append(sep);
+            sb.append(JSON.toJSON(t));
+            sep = ",";
+        }
+        sb.append(']');
+        return sb.toString();
+    }
+
+    public Object koData() {
+        return toArray();
+    }
+
+    private void notifyChange() {
+        Bindings m = model;
+        if (m != null) {
+            m.valueHasMutated(name);
+            for (String dependant : deps) {
+                m.valueHasMutated(dependant);
+            }
+        }
+        Runnable r = onchange;
+        if (r != null) {
+            r.run();
+        }
+    }
+
+    @Override
+    public JSONList clone() {
+        JSONList ko = (JSONList) super.clone();
+        ko.model = null;
+        return ko;
     }
 }
