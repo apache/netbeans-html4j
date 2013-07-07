cf8cdda9dfa5d0b14be299557fe75a6b273d2e52
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-07-07 08:31:14 +0200

    Initial version of javadoc for the Geo API

--- geo/src/main/java/net/java/html/geo/OnLocation.java
+++ geo/src/main/java/net/java/html/geo/OnLocation.java
@@ -25,14 +25,49 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
+/** Generates a handle which can configure, {@link Position.Handle#start() start}
+ * and {@link Position.Handle#stop() stop} requests for obtaining current
+ * location of the application/device/user. Put the {@link OnLocation} annotation
+ * on top of a (non-private) method in your class which takes one argument
+ * {@link Position}. Based on name of your method (unless a class name is
+ * directly specified via {@link #className()} attribute) a handle class is
+ * generated. For example if your method name is <code>callMeWhenYouKnowWhereYouAre</code>
+ * a package private class <code>CallMeWhenYouKnowWhereYouAreHandle</code> will
+ * be generated in the same package. One can use its <code>createQuery</code>
+ * and <code>createWatch</code> static method to get one time/repeated time
+ * instance of a {@link Position.Handle handle}. After configuring the
+ * {@link Position.Handle handle} via its setter methods, one can 
+ * {@link Position.Handle#start() start} the location request.
+ * <p>
+ * In case something goes wrong a method in the same class named {@link #onError()}
+ * can be specified (should take one {@link Exception} parameter). 
+ * <p>
+ * The overall behavior of the system mimics <a href="http://www.w3.org/TR/2012/PR­geolocation­API­20120510/">
+ * W3C's Geolocation API</a>.
  *
  * @author Jaroslav Tulach <jtulach@netbeans.org>
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface OnLocation {
+    /** Name of the {@link Position.Handle handle} class to generate. By
+     * default the name is derived from the name of annotated method by
+     * capitalizing the first character and appending <code>Handle</code>.
+     * <p>
+     * The generated class contains two static methods: <code>createQuery</code>
+     * and <code>createWatch</code> which take no parameters if this method 
+     * is static or one parameter (of this class) if this method is instance
+     * one. Both static methods return {@link Position.Handle}.
+     * 
+     * @return string suitable for a new class in the same package
+     */
     public String className() default "";
     
+    /** Name of a method in this class which should be called in case of 
+     * an error. The method has to be non-private and take {@link Exception} 
+     * parameter. By default the exception printed to console.
+     * 
+     * @return name of method in this class
+     */
     public String onError() default "";
 }
--- geo/src/main/java/net/java/html/geo/Position.java
+++ geo/src/main/java/net/java/html/geo/Position.java
@@ -47,8 +47,10 @@ public final class Position {
         private Double speed;
     }
 
-    /**
-     *
+    /** Rather than subclassing this class directly consider using {@link OnLocation}
+     * annotation. Such annotation will generate a subclass for you automatically
+     * with two static methods <code>createQuery</code> and <code>createWatch</code>
+     * which can be used to obtain instance of this class.
      */
     public static abstract class Handle {
 
@@ -57,30 +59,63 @@ public final class Position {
         private long timeout;
         private long maximumAge;
 
+        /** Creates new instance of this handle.
+         * 
+         * @param oneTime <code>true</code> if the handle represents one time 
+         *   <em>query</em>. <code>false</code> if it represents a <em>watch</em>
+         */
         protected Handle(boolean oneTime) {
             super();
             this.oneTime = oneTime;
         }
 
+        /** Callback from the implementation when a (new) position has been
+         * received and identified
+         * @param p the position
+         * @throws Throwable if an exception is thrown, it will be logged by the system
+         */
         protected abstract void onLocation(Position p) throws Throwable;
 
-        protected abstract void onError(Throwable t) throws Throwable;
+        /** Callback when an error occurs.
+         * @param ex the exception describing what went wrong
+         * @throws Throwable if an exception is thrown, it will be logged by the system
+         */
+        protected abstract void onError(Exception ex) throws Throwable;
 
+        /** Turns on high accuracy mode as specified by the 
+         * <a href="http://www.w3.org/TR/2012/PR­geolocation­API­20120510/">
+         * W3C's Geolocation API</a>. By default the mode is disabled.
+         * @param enable <code>true</code> or <code>false</code>
+         */
         public void setHighAccuracy(boolean enable) {
             this.enableHighAccuracy = enable;
         }
 
+        /** The amount of milliseconds to wait for a result.
+         * By default infinity.
+         * @param timeout time in milliseconds to wait for a result.
+         */
         public void setTimeout(long timeout) {
             this.timeout = timeout;
         }
 
+        /** Sets maximum age of cached results which are acceptable to be
+         * returned. By default maximum age is set to zero.
+         * @param age time in milliseconds of acceptable cached results
+         */
         public void setMaximumAge(long age) {
             this.maximumAge = age;
         }
 
+        /** Initializes the <em>query</em> or <em>watch</em> request(s) and
+         * returns immediately.
+         */
         public void start() {
         }
 
+        /** Stops all pending requests. After this call no further callbacks
+         * can be obtained.
+         */
         public void stop() {
         }
     }
