8bff1684f5d73919c0c18f9d49200df25d66eaa8
Jaroslav Tulach <jaroslav.tulach@netbeans.org>
2014-03-26 18:27:36 +0100

    Explaining the meaning of OnPropertyChange and providing link to an online sample that is using it

--- json/src/main/java/net/java/html/json/OnPropertyChange.java
+++ json/src/main/java/net/java/html/json/OnPropertyChange.java
@@ -47,8 +47,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** Defines a method that is supposed to be notified when a 
- * property defined by {@link Model} has been changed.
+/** Marks a method that is going to be notified when a 
+ * property defined by {@link Model} has been changed. This is
+ * especially useful when one wants to react to changes in the 
+ * model caused by the rendered view. In case of 
+ * <a href="http://knockoutjs.com">knockout.js</a> technology
+ * one could for example react to selection of a name from a combo
+ * box:
+ * <pre>
+ * 
+ * &lt;!-- associates the selected value with property <em>name</em> --&gt;
+ * 
+ * &lt;select data-bind="value: name"&gt;
+ *   &lt;option&gt;Jiří&lt;/option&gt;
+ *   &lt;option&gt;Jarda&lt;/option&gt;
+ *   &lt;option&gt;Petr&lt;/option&gt;
+ *   &lt;option&gt;Tomáš&lt;/option&gt;
+ * &lt;/select&gt;
+ * 
+ * // Java code snippet reacting to change of the <em>name</em> property:
+ * 
+ * {@link OnPropertyChange @OnPropertyChange}("name") 
+ * <b>static void</b> propertyChanged(AModel inst, {@link String} propertyName) {
+ *   // schedule some operation
+ *   // on the model
+ * }
+ * </pre>
+ * The method's first argument should be the instance of the 
+ * associated {@link Model model class}.
+ * There can be an optional second {@link String} argument which will be set
+ * to the name of the changed property. The second argument is only useful when
+ * a single method reacts to changes in multiple properties.
+ * <p>
+ * An online example using this technique is 
+ * <a target="_blank" href="http://dew.apidesign.org/dew/#7138581">available here</a> -
+ * it observes selection in a combo box and in case it changes 
+ * the example sends a network
+ * request and {@link net.java.html.json.OnReceive asynchronously updates}
+ * list of code snippets.
  *
  * @author Jaroslav Tulach <jtulach@netbeans.org>
  */
