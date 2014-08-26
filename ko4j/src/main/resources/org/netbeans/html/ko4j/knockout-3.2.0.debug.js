647dd9ec1534c749101f96c92a351d6435b70805
Jaroslav Tulach <jtulach@netbeans.org>
2014-08-26 18:27:14 +0200

    Turn off the debug mode

--- ko4j/src/main/resources/org/netbeans/html/ko4j/knockout-3.2.0.debug.js
+++ ko4j/src/main/resources/org/netbeans/html/ko4j/knockout-3.2.0.debug.js
@@ -48,7 +48,7 @@
  */
 
 (function(){
-var DEBUG=true;
+var DEBUG=false;
 (function(undefined){
     // (0, eval)('this') is a robust way of getting a reference to the global object
     // For details, see http://stackoverflow.com/questions/14119988/return-this-0-evalthis/14120023#14120023
