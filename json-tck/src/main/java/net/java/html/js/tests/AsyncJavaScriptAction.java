0367bae894a9c94a73315769d8e4922ae896eb71
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2022-04-30 08:53:54 +0200

    Avoid using HashSet and ArrayList

--- json-tck/src/main/java/net/java/html/js/tests/AsyncJavaScriptAction.java
+++ json-tck/src/main/java/net/java/html/js/tests/AsyncJavaScriptAction.java
@@ -19,17 +19,16 @@
 package net.java.html.js.tests;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 import net.java.html.js.JavaScriptBody;
 import static net.java.html.js.tests.JavaScriptBodyTest.assertEquals;
 import static net.java.html.js.tests.JavaScriptBodyTest.assertTrue;
 import static net.java.html.js.tests.JavaScriptBodyTest.fail;
+import net.java.html.json.Models;
 
 final class AsyncJavaScriptAction {
-    private List<Integer> collected = new java.util.ArrayList<>();
+    private final List<Integer> collected = Models.asList();
     private boolean successStoringLater;
     private AssertionError invertedVia;
 
@@ -96,9 +95,9 @@ final class AsyncJavaScriptAction {
     private static void assertSequenceButN(List<Integer> data, int from, int upto, int allowedDisorder) {
         int count = upto - from;
         assertEquals(data.size(), count, "all items: " + data);
-        Set<Integer> all = new HashSet<>(data);
+        Collection<Integer> all = Models.asList(data.toArray(new Integer[0]));
         Integer prev = null;
-        for (int i = from, at = 0; i < upto; i++, at++) {
+        for (Integer i = from, at = 0; i < upto; i++, at++) {
             final Integer atI = data.get(at);
             if (prev != null && prev < atI) {
                 if (--allowedDisorder < 0) {
